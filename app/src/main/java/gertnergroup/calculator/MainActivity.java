722a6e94f338b1b88b708c6d3b9c29e02f83fcf7
Sunny3oy <meisunny1992@hotmail.com>
2016-06-14 17:19:56 -0400

    Added comments in MainActivity.java

--- app/src/main/java/gertnergroup/calculator/MainActivity.java
+++ app/src/main/java/gertnergroup/calculator/MainActivity.java
@@ -12,16 +12,19 @@ import android.widget.TextView;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    //widgets and text declearations
     private Button plus,sub,muti,div;
     private TextView result;
     private EditText doge1,doge2;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        init();
+        init();//user defined function to hold all inits
     }
 
     private void init() {
+        //linking each corresponding id to the correct names
         plus = (Button)findViewById(R.id.plus);
         sub = (Button)findViewById(R.id.sub);
         muti = (Button)findViewById(R.id.muti);
@@ -29,19 +32,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         result = (TextView)findViewById(R.id.result);
         doge1 = (EditText)findViewById(R.id.doge1);
         doge2 = (EditText)findViewById(R.id.doge2);
-
+        //these are the buttons, they dont do anything until you click on them
         plus.setOnClickListener(this);
         sub.setOnClickListener(this);
         muti.setOnClickListener(this);
         div.setOnClickListener(this);
     }
+    //once a button is pressed, this function will check which button id was clicked
     public void onClick(View v) {
-        String number1 = doge1.getText().toString();
+        String number1 = doge1.getText().toString();//changing from a text to string variable
         String number2 = doge2.getText().toString();
+        //below is a switch statement
     switch (v.getId()){
         case R.id.plus:
-            double adding = Double.parseDouble(number1)+Double.parseDouble(number2);
-            result.setText(String.valueOf(adding));
+            double adding = Double.parseDouble(number1)+Double.parseDouble(number2);//we parse our
+            // string into a double so we can do math with it
+            result.setText(String.valueOf(adding));//we use this function "String.valueOf" to convert our
+            // double value back to a string value and then using the setText function to "cout"to results
             break;
         case R.id.sub:
             double sub = Double.parseDouble(number1)-Double.parseDouble(number2);
@@ -52,6 +59,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             result.setText(String.valueOf(muti));
             break;
         case R.id.div:
+            //below if statement check if the user inputs 0 as the 2nd doge number.
             if(Objects.equals(number2, "0")) {
                 result.setText("Doge cannot divide by 0, Doge will explode, such danger!");
                 break;
